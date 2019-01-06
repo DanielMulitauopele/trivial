9603c46be4905f8df7a94da0d298bbaa61935017
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-05 17:59:58 -0700

    Compiles successfully

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {}
-
+class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello Daniel! Lets get this bread.
           </p>
           <a
             className="App-link"
