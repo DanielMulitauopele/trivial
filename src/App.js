b7b6ad35f1ff2e3cfb34d307fe083b27d01fa843
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-06 15:23:50 -0700

    Changes back to p tag, so that Travis passes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1>
             Trivial
           </h1>
-          <a>Reach the Top</a>
+          <p>Reach the Top</p>
         </header>
       </div>
     );
