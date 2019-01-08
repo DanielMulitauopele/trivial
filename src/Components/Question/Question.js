8ecfe67f047c9047fe88d84dde4c10dbc626e770
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-07 21:05:54 -0700

    Attempts to pull in JSON data in the index file, not yet passing data

---
+++ src/Components/Question/Question.js
@@ -0,0 +1,6 @@
+category: {props.category},
+type: {props.type}
+difficulty: {props.difficulty}
+question: {props.question}
+correct_answer: {props.correct_answer}
+incorrect_answers: {props.incorrect_answers}
