04ac9e7b3b5e02cc6c6bd4170f85e5209c0b3e11
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-07 22:47:27 -0700

    Returns json data all the way to the app, now need to send it back down to the QuestionContainer to render appropriately.

--- src/Components/QuestionContainer/QuestionContainer.js
+++ src/Components/QuestionContainer/QuestionContainer.js
@@ -13,16 +13,16 @@ class QuestionContainer extends Component {
     let questions = props.results.map((question, index) => {
       return(
          <div key={index}>
-         <h3>{question.question}</h3>
-         <p>{question.category}</p>
-         <p>{question.difficulty}</p>
+           <h3>{question.question}</h3>
+           <p>{question.category}</p>
+           <p>{question.difficulty}</p>
 
-         <ul>
-           <li>{question.correct_answer}</li>
-           <li>{question.correct_answer}</li>
-           <li>{question.correct_answer}</li>
-           <li>{question.correct_answer}</li>
-         </ul>
+           <ul>
+             <li>{question.correct_answer}</li>
+             <li>{question.correct_answer}</li>
+             <li>{question.correct_answer}</li>
+             <li>{question.correct_answer}</li>
+           </ul>
          </div>
        )
     })
