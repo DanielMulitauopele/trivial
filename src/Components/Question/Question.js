5a6119dcd3f14aadba883ba4d5c48c530ba3c36e
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-08 23:08:27 -0700

    Adds important tag to position fixed and background size tags. adds grid display to answers

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -4,11 +4,11 @@ import './Question.css';
 const Question = (props, index) => {
   return(
     <div key={index} className="container">
-      <h3>{props.question.question}</h3>
-      <p>{props.question.category}</p>
-      <p>{props.question.difficulty}</p>
+      <h3 className="question">{props.question.question}</h3>
+      <p className="category">{props.question.category}</p>
+      <p className="difficulty">{props.question.difficulty}</p>
 
-      <ul>
+      <ul className="answers">
         <li>{props.question.correct_answer}</li>
         <li>{props.question.correct_answer}</li>
         <li>{props.question.correct_answer}</li>
