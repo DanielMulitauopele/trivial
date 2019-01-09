5d0675bee0f1720702b31c20716660fd4996c96a
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-01-08 23:08:27 -0700

    Adds important tag to position fixed and background size tags. adds grid display to answers

--- src/Utils/API/index.js
+++ src/Utils/API/index.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 class TriviaJSON extends Component {
   getQuestions = async () => {
-    const data = await fetch('https://opentdb.com/api.php?amount=3&category=&difficulty=&type=')
+    const data = await fetch('https://opentdb.com/api.php?amount=50&difficulty=hard&type=multiple')
     const response = await data.json()
     return response.results
   }
